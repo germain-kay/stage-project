419fddff92a564b00a703e779b33c1a21e8543ea
Germain <germainkay@hotmail.fr>
2025-01-06 16:55:34 +0100

    Ajout de Phosphor-icons + Hello World

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,20 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { AppWindow } from "@phosphor-icons/react";
 import './App.css'
 
 const App = () => {
     console.log('Hello from component')
-    const now = new Date()
-    const a = 10
-    const b = 20
 
     return (
         <div>
-            <p>Hello world</p>
-            <p>
-                {a} plus {b} is {a + b}
-            </p>
+            <AppWindow size={32} weight="fill"/>
+        <p>Hello world</p>
         </div>
     )
 }
