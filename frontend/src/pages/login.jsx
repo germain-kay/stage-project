0a5d5e6d3b3b5ae2feef822d10903c0159a9f277
Germain <germainkay@hotmail.fr>
2025-02-12 19:02:58 +0100

    change logo

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
 import { useTranslation } from 'react-i18next';
+import { TextIndent  } from "@phosphor-icons/react";
 
 function Page() {
     const {t    } = useTranslation();
@@ -8,11 +9,7 @@ function Page() {
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <img
-                        alt="Your Company"
-                        src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                        className="mx-auto h-10 w-auto"
-                    />
+                    < TextIndent className="bg-black text-white mx-auto h-10 w-auto"/>
                     <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
                         {t('login.title')}
                     </h2>
